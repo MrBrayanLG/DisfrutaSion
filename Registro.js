1a6ad45deda6da0e4e23e4a23e565d11a04f46cf
MrBrayanLG <140257006+MrBrayanLG@users.noreply.github.com>
2023-11-26 22:02:43 -0500

    Add files via upload

---
+++ Registro.js
@@ -0,0 +1,76 @@
+
+function guardarUsuario() {
+    
+    var nombre = document.getElementById("nombres").value;
+    var apellido = document.getElementById("apellidos").value;
+    var correo = document.getElementById("correo").value;
+    var contraseña = document.getElementById("password").value;
+    var genero = document.getElementById("genero").value;
+
+    
+    var usuario = {
+        nombre: nombre,
+        apellido: apellido,
+        correo: correo,
+        contraseña: contraseña,
+        genero: genero
+    };
+
+    
+    var usuarioJSON = JSON.stringify(usuario);
+    localStorage.setItem('usuario', usuarioJSON);
+    localStorage.setItem('nombreUsuario', nombre);
+    window.location.href = 'index.html';
+}
+
+function iniciarSesion() {
+    
+    var correo = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var usuarioJSON = localStorage.getItem('usuario');
+
+    if (usuarioJSON) {
+        
+        var usuario = JSON.parse(usuarioJSON);
+
+       
+        if (correo === usuario.correo && password === usuario.contraseña) {
+            
+            window.location.href = 'Disfruta-Sión-Inicio.html';
+        } else {
+           
+            mostrarMensajeDeAlerta('Cuenta no registrada o datos incorrectos.');
+        }
+    } else {
+       
+        mostrarMensajeDeAlerta('Cuenta no registrada o datos incorrectos.');
+    }
+}
+
+
+const nombreUsuario = localStorage.getItem('nombreUsuario');
+if (nombreUsuario) {
+    const perfilLink = document.querySelector(".enlace-perfil");
+    perfilLink.textContent = `PERFIL (${nombreUsuario})`;
+}
+
+
+
+
+function mostrarPopup(mensaje) {
+    document.getElementById('popup-message').innerText = mensaje;
+    document.getElementById('popup').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+}
+
+function cerrarPopup() {
+    document.getElementById('popup').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+}
+
+function mostrarMensajeDeAlerta(mensaje) {
+    mostrarPopup(mensaje);
+}
+
+
+
---
+++ inicio.js
@@ -0,0 +1,70 @@
+function toggleOptions() {
+    var optionsContainer = document.getElementById("optionsContainer");
+    optionsContainer.style.display = (optionsContainer.style.display === "block") ? "none" : "block";
+}
+
+function goToProfile() {
+    alert("Redirigiendo al perfil...");
+}
+
+function cerrarSesion() {
+    window.location.href = 'index.html';
+
+}
+document.addEventListener("DOMContentLoaded", function () {
+    const nombreUsuario = localStorage.getItem('nombreUsuario');
+    if (nombreUsuario) {
+        const saludoUsuarioElement = document.getElementById('saludo-usuario');
+        if (saludoUsuarioElement) {
+            saludoUsuarioElement.textContent = `¡Bienvenido, ${nombreUsuario}!`;
+        }
+    }
+});
+
+
+
+
+
+
+
+window.onload = function () {
+    cargarReseñas();
+};
+
+function agregarReseña() {
+    var nombre = document.getElementById('nombre').value;
+    var comentario = document.getElementById('reseña').value;
+
+    if (nombre && comentario) {
+        var nuevaReseña = document.createElement('div');
+        nuevaReseña.innerHTML = '<div class="resena"><h3 class="cliente-nombre">' + nombre + '</h3><p class="cliente-resena">' + comentario + '</p></div>';
+
+        document.getElementById('resenas-container').appendChild(nuevaReseña);
+
+        document.getElementById('nombre').value = '';
+        document.getElementById('reseña').value = '';
+
+        guardarReseña(nombre, comentario);
+    } else {
+        alert('Por favor, completa ambos campos antes de enviar la reseña.');
+    }
+}
+
+function guardarReseña(nombre, comentario) {
+    var reseñasGuardadas = JSON.parse(localStorage.getItem('reseñas')) || [];
+
+    reseñasGuardadas.push({ nombre: nombre, comentario: comentario });
+
+    localStorage.setItem('reseñas', JSON.stringify(reseñasGuardadas));
+}
+
+function cargarReseñas() {
+    var reseñasGuardadas = JSON.parse(localStorage.getItem('reseñas')) || [];
+
+    var contenedorReseñas = document.getElementById('resenas-container');
+    reseñasGuardadas.forEach(function (reseña) {
+        var nuevaReseña = document.createElement('div');
+        nuevaReseña.innerHTML = '<div class="resena"><h3 class="cliente-nombre">' + reseña.nombre + '</h3><p class="cliente-resena">' + reseña.comentario + '</p></div>';
+        contenedorReseñas.appendChild(nuevaReseña);
+    });
+}

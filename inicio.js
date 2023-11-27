63301848bb8020242c68b90f943c03c36d9092bf
MrBrayanLG <140257006+MrBrayanLG@users.noreply.github.com>
2023-11-26 22:30:31 -0500

    se agregó inicio.js

--- inicio.js
+++ inicio.js
@@ -1,25 +1,4 @@
-function toggleOptions() {
-    var optionsContainer = document.getElementById("optionsContainer");
-    optionsContainer.style.display = (optionsContainer.style.display === "block") ? "none" : "block";
-}
 
-function goToProfile() {
-    alert("Redirigiendo al perfil...");
-}
-
-function cerrarSesion() {
-    window.location.href = 'index.html';
-
-}
-document.addEventListener("DOMContentLoaded", function () {
-    const nombreUsuario = localStorage.getItem('nombreUsuario');
-    if (nombreUsuario) {
-        const saludoUsuarioElement = document.getElementById('saludo-usuario');
-        if (saludoUsuarioElement) {
-            saludoUsuarioElement.textContent = `¡Bienvenido, ${nombreUsuario}!`;
-        }
-    }
-});
 
 
 
